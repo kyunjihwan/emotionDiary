c0ac6687b3b0698a74626160e1189f0affc45dd0
DESKTOP-1NFVDUO\ishift <j10345678@gmail.com>
2023-10-23 16:44:43 +0900

    Router를 사용해서 SPA 만들어보기

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RouteTest from "./components/RouteTest";
+
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+
+function App() {
+  return (
+    // url 매핑 처리를 위해 BrowserRouter를 사용한다.
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <RouteTest />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
