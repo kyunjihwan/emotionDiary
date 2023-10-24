bd6eebbeff65bb652964b9010505a18c7889ee64
DESKTOP-1NFVDUO\ishift <j10345678@gmail.com>
2023-10-24 10:20:14 +0900

    프로젝트 기초 공사 2 - 상태 관리 세팅하기

--- src/App.js
+++ src/App.js
@@ -5,63 +5,93 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
+import React, { useReducer, useRef } from "react";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => {
+        return it.id !== action.targetId;
+      });
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) => {
+        return it.id === action.targetId ? { ...it, ...action.data } : it;
+      });
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
-  return (
-    // url 매핑 처리를 위해 BrowserRouter를 사용한다.
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton
-              text={"왼쪽 버튼"}
-              onClick={() => {
-                alert("왼쪽 클릭");
-              }}
-            />
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽 버튼"}
-              onClick={() => {
-                alert("오른쪽 클릭");
-              }}
-            />
-          }
-        />
-        <h2>App.js</h2>
+  const [data, dispatch] = useReducer(reducer, []);
 
-        <MyButton
-          text={"버튼"}
-          type={"positive"}
-          onclick={() => {
-            alert("버튼 클릭");
-          }}
-        />
-        <MyButton
-          text={"버튼"}
-          type={"negative"}
-          onclick={() => {
-            alert("버튼 클릭");
-          }}
-        />
-        <MyButton
-          text={"버튼"}
-          onclick={() => {
-            alert("버튼 클릭");
-          }}
-        />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+  const dataId = useRef(1);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current++;
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId,
+    });
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      targetId,
+      data: {
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider vlaue={{ onCreate, onRemove, onEdit }}>
+        <BrowserRouter>
+          <div className="App">
+            <h2>App.js</h2>
+
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
