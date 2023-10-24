1a1a28036836ad5c172a63d7afcb0105923d188e
DESKTOP-1NFVDUO\ishift <j10345678@gmail.com>
2023-10-24 09:49:31 +0900

    프로젝트 기초 공사1 - 폰트 셋팅, 레이아웃 셋팅, 이미지 셋팅, 공통 컴포넌트 셋팅

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
-
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     // url 매핑 처리를 위해 BrowserRouter를 사용한다.
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => {
+                alert("왼쪽 클릭");
+              }}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => {
+                alert("오른쪽 클릭");
+              }}
+            />
+          }
+        />
         <h2>App.js</h2>
-        <RouteTest />
+
+        <MyButton
+          text={"버튼"}
+          type={"positive"}
+          onclick={() => {
+            alert("버튼 클릭");
+          }}
+        />
+        <MyButton
+          text={"버튼"}
+          type={"negative"}
+          onclick={() => {
+            alert("버튼 클릭");
+          }}
+        />
+        <MyButton
+          text={"버튼"}
+          onclick={() => {
+            alert("버튼 클릭");
+          }}
+        />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/new" element={<New />} />
